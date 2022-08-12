7b606af7ffe5037df2b4535386605c8fc0bfdc7b
priyanshu6874 <88717278+priyanshu6874@users.noreply.github.com>
2022-08-12 16:12:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import random;
+
+response ="y"
+while response=="y":
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    if no == 2:
+        print("[-----]")
+        print("[     ]")
+        print("[ 0 0 ]")
+        print("[     ]")
+        print("[-----]")
+    if no == 3:
+        print("[-----]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[-----]")
+    if no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+
+    response=input("enter "y" to continue and "n"to exit")
